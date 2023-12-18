3643e02ed07f552a40e4e3a3b975ad98608b568c
김진기_T6039 <50827658+jingi-data@users.noreply.github.com>
2023-12-18 10:31:52 +0900

    'collate_fn' 관련 스크립트 큰 변경 및 wandb 기능 추가 (#25)
    * fix - 'collate_fn' 관련 모델 수정
    
    - 'data_loader.py' 및 'model.py' 재작성
    - 경로 재설정
    
    close #23
    
    * feat - wandb 추가
    
    - default 모델을 'team-lucid/deberta-v3-base-korean'으로 변경
    - wandb 로그를 commit하지 않도록 '.gitignore' 갱신
    
    close #24
    
    close #25

--- model.py
+++ model.py
@@ -3,62 +3,49 @@ import random
 
 import pandas as pd
 
-from tqdm.auto import tqdm
-import pytorch_lightning as pl
-import transformers
 import torch
-import torchmetrics
+import pytorch_lightning as pl
+from transformers import AutoModelForSequenceClassification
+from torchmetrics.functional import pearson_corrcoef
 
 
 class Model(pl.LightningModule):
     def __init__(self, model_name, lr):
         super().__init__()
         self.save_hyperparameters()
-
-        self.model_name = model_name
+        self.model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=1)
         self.lr = lr
-
-        # 사용할 모델을 호출합니다.
-        self.plm = transformers.AutoModelForSequenceClassification.from_pretrained(
-            pretrained_model_name_or_path=model_name, num_labels=1)
-        # Loss 계산을 위해 사용될 L1Loss를 호출합니다.
         self.loss_func = torch.nn.L1Loss()
 
-    def forward(self, x):
-        x = self.plm(x)['logits']
+    def forward(self, **inputs):
+        outputs = self.model(**inputs)
+        return outputs.logits.squeeze(-1)
 
-        return x.squeeze(-1)
+    def step(self, batch):
+        inputs = {key: val for key, val in batch.items() if key!= 'labels'}
+        labels = batch['labels']
+        predictions = self(**inputs)
+        loss = self.loss_func(predictions, labels.float())
+        pearson = pearson_corrcoef(predictions, labels.float())
+        return loss, pearson
 
     def training_step(self, batch, batch_idx):
-        x, y = batch
-        logits = self(x)
-        loss = self.loss_func(logits, y.float())
-        self.log("train_loss", loss)
-
+        loss, _ = self.step(batch)
+        self.log("train_loss", loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
         return loss
 
     def validation_step(self, batch, batch_idx):
-        x, y = batch
-        logits = self(x)
-        loss = self.loss_func(logits, y.float())
-        self.log("val_loss", loss)
-
-        self.log("val_pearson", torchmetrics.functional.pearson_corrcoef(logits.squeeze(), y.squeeze()))
-
-        return loss
+        loss, pearson = self.step(batch)
+        self.log("val_loss", loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
+        self.log("val_pearson", pearson, on_step=True, on_epoch=True, prog_bar=True, logger=True)
 
     def test_step(self, batch, batch_idx):
-        x, y = batch
-        logits = self(x)
-
-        self.log("test_pearson", torchmetrics.functional.pearson_corrcoef(logits.squeeze(), y.squeeze()))
-
+        loss, pearson = self.step(batch)
+        self.log("test_pearson", pearson, logger=True)
+        
     def predict_step(self, batch, batch_idx):
-        x = batch
-        logits = self(x)
-
-        return logits.squeeze()
+        inputs = {key: val for key, val in batch.items() if key!= 'labels'}
+        return self(**inputs)
 
     def configure_optimizers(self):
-        optimizer = torch.optim.AdamW(self.parameters(), lr=self.lr)
-        return optimizer
+        return torch.optim.AdamW(self.parameters(), lr=self.lr)
