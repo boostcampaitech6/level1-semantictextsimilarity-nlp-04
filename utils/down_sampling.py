e921ec3a70a69e85590b1aa9c30a4a2455d9803d
kooqooo <koo9717@gmail.com>
2023-12-19 12:32:34 +0900

    feature - `down_sampling.py` 생성
    #33

---
+++ utils/down_sampling.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+
+
+def delete_random(df, column, value, perc):
+    np.random.seed(0)
+    mask = df[column] == value
+    indices = df[mask].index
+    remove_n = int(perc * len(indices))
+    drop_indices = np.random.choice(indices, remove_n, replace=False)
+    df = df.drop(drop_indices)
+    return df
+
+
+def count_zeroes(df):
+    return (df['label']==0.0).value_counts()[True]
+
+
+train = pd.read_csv('./data/train.csv')
+
+print('Before : ', count_zeroes(train))
+dropped = delete_random(train, 'label', 0.0, 0.5)
+print('After : ', count_zeroes(dropped))
+
+dropped.to_csv('./data/dropped_train.csv', sep=',', encoding='utf-8', index=False)
